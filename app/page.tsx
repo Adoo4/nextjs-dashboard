def59ce41b7d1a4b8bd7521e8ee5e68b5f3bd1fa
User Name <email>
2025-03-22 00:54:54 +0100

    nextJS3

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import { ArrowRightIcon } from "@heroicons/react/24/outline";
 import styles from "@/app/ui/home.module.css";
 import Link from "next/link";
 import LikeButton from './like-button'
-import { lusitana } from "./ui/fonts";
+
 import Image from 'next/image'
 
 export default function Page() {
@@ -22,7 +22,7 @@ export default function Page() {
             , brought to you by Vercel.
           </p>
         
-          <p className={lusitana.className}>Adnan Bajraktarevic</p>
+          <p >Adnan Bajraktarevic</p>
           <Link
             href="/login"
             className="flex items-center gap-5 self-start rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
